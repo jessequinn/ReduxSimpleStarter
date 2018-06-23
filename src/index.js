7e4d8ff8d02b7a90f60eb598ecda8205224bcf3a
Jesse Quinn <me@jessequinn.info>
2018-06-23 19:47:03 -0300

    removed Google API

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-const API_KEY = 'AIzaSyDEW643YWie5x10b9KCeZ4UK9vO5PDNGhM';
+const API_KEY = '#';
 
 class App extends Component {
   constructor(props) {
